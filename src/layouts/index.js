e1bcd12015919712cbd931f585b4bee29a16b819
avikola <avishkar1995@gmail.com>
2020-01-02 01:49:44 -0800

    Added Site Metadata

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,9 +2,15 @@ import React from "react"
 
 import { AllButtons } from "../components/home/allbuttons"
 import { Intro } from "../components/home/intro"
+import { Helmet } from "react-helmet"
 
 export default ({ children }) => (
   <div className="full_home_site">
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Avishkar Kolahalu // Software Developer</title>
+      <link rel="canonical" href="https://avikola.com" />
+    </Helmet>
     <AllButtons />
     <Intro introClass="intro-text" />
     {children}
