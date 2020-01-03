8aa02639a02ca26bc1b07d7098d0186b189de4a7
avikola <avishkar1995@gmail.com>
2020-01-02 18:02:07 -0800

    Created scrolling layout for mobile

--- src/components/home/allbuttons.js
+++ src/components/home/allbuttons.js
@@ -5,10 +5,12 @@ import styles from "../../styles/home/allbuttons.module.scss"
 // prettier-ignore
 export const AllButtons = () => {
   return (
+    <div className={styles.hideit}>
     <div className={styles.home_buttons_div}>
       <HomeButton label="about" num="1" />
       <HomeButton label="projects" num="2" />
       <HomeButton label="experience" num="3" />
+    </div>
     </div>
   )
 }
